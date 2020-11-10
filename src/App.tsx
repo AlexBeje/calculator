e60b4437de626e13bf2a5d5f9171b7c5df435e1a
Alex <alex.bejenaru93@gmail.com>
2020-11-10 09:21:38 +0100

    📦 NEW: Added calculator functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const operatorsList = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    ".",
+    "+",
+    "-",
+    "*",
+    "/",
+    "=",
+  ];
+
+  const extraOperatorsList = ["(", ")", "C", "AC"];
+
+  const [result, setResult] = useState<any>("");
+
+  const operatorsRenderer = operatorsList.map((operator, i) => {
+    return (
+      <button
+        className="app__button"
+        onClick={(event) => resultHandler(event)}
+        value={operator}
+        key={operator + i}
+      >
+        {operator}
+      </button>
+    );
+  });
+
+  const extraOperatorsRenderer = extraOperatorsList.map((extraOperator, i) => {
+    return (
+      <button
+        className="app__button"
+        onClick={(event) => resultHandler(event)}
+        value={extraOperator}
+        key={extraOperator + i}
+      >
+        {extraOperator}
+      </button>
+    );
+  });
+
+  const resultHandler = (event: any) => {
+    const clickedKeyValue = event.target.value;
+
+    switch (clickedKeyValue) {
+      case "=":
+        /* eslint no-eval: 0 */
+        setResult(String(eval(result)));
+        break;
+      case "C":
+        if (result !== "") {
+          setResult(result.substring(0, result.length - 1));
+        }
+        break;
+      case "AC":
+        setResult("");
+        break;
+      default:
+        setResult(result + clickedKeyValue);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>RESULT: {result}</div>
+      <div>{operatorsRenderer}</div>
+      <div>{extraOperatorsRenderer}</div>
     </div>
   );
 }
